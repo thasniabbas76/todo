3d4bdf06b30d847e54cba90518d427b2fe2c2b4c
Thasni Abbas <thasniabbas76@gmail.com>
2025-04-03 23:45:25 +0530

    created an app, then views,py and urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("",include("todoapp.urls")),
     path("admin/", admin.site.urls),
+    
 ]
