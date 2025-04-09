c0a3aa5cd0be97c2bc56fad680d9fae3c7c6b004
Thasni Abbas <thasniabbas76@gmail.com>
2025-04-10 00:13:17 +0530

    edit views

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -6,11 +6,20 @@ from .models import Task
 def index(request):
     if request.method == 'POST':
         title = request.POST.get('title')
-        description = request.POST.get('desccription')
+        description = request.POST.get('description')
         status = request.POST.get('status', 'PENDING')
 
         if title and description:
             Task.objects.create(title=title, description=description)
             return redirect('index')
-        tasks = Task.objects.all()
+    tasks = Task.objects.all()
     return render(request, "index.html", {'tasks':tasks})
+def edit(request,id):
+    task = get_object_or_404(Task,id=id)
+
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        task.status = request.POST.get('status','PENDING')
+        task.save()
+        return redirect('index')
